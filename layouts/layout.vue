2e9600bd17df8157b3fc5eb7ab3fbd749cf91225
masato1999 <1999i.masato@gmail.com>
2023-04-30 15:23:01 +0900

    一時避難

--- layouts/layout.vue
+++ layouts/layout.vue
@@ -7,18 +7,18 @@
 </template>
 
 <style lang="scss" scoped>
-@import './style';
+@import "./style";
 </style>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
-import Header from '@/components/organisms/Header/main.vue'
-import Sidebar from '@/components/organisms/Sidebar/main.vue'
+import { defineComponent } from "@vue/composition-api";
+import Header from "@/components/organisms/Header/main.vue";
+import Sidebar from "@/components/organisms/Sidebar/main.vue";
 
 export default defineComponent({
   components: {
     Header,
     Sidebar,
   },
-})
+});
 </script>
