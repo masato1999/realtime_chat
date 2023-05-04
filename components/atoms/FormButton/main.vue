e0545db9b722069e300e5b2aad718a83700e345c
masato1999 <1999i.masato@gmail.com>
2023-05-04 21:14:29 +0900

    検索ボタン作成

---
+++ components/atoms/FormButton/main.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="FormButton">
+    <button class="FormButton__Container" @click="onClick">
+      <slot />
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, SetupContext } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  setup(_, context: SetupContext) {
+    const onClick = () => {
+      console.log("FormButton: onClick");
+      context.emit("click");
+    };
+
+    return {
+      onClick,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import "./style";
+</style>
--- components/organisms/Header/main.vue
+++ components/organisms/Header/main.vue
@@ -1,6 +1,22 @@
 <template>
-  <div class="Header" />
+  <div class="Header">
+    <FormButton>検索</FormButton>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api";
+import FormButton from "@/components/atoms/FormButton/main.vue";
+
+export default defineComponent({
+  components: {
+    FormButton,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
 
 <style lang="scss" scoped>
 @import "./style";
