4eeea3357a033eac9b72e6b2799eac9384aed75a
masato1999 <1999i.masato@gmail.com>
2023-06-03 21:40:59 +0900

    firebaseとの接続

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,8 @@
 import { defineComponent, provide } from "@nuxtjs/composition-api";
 import { UserInfo } from "./types";
 import { userInfoKey, messageListKey } from "./store";
+import { database } from "@/plugins/firebase";
+import { push, onValue } from "@firebase/database";
 import ChatField from "@/components/organisms/ChatField/main.vue";
 
 export default defineComponent({
@@ -29,6 +31,14 @@ export default defineComponent({
       "ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。",
       "ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。",
     ];
+
+    push(database, "データを送ります")
+      .then((response) => {
+        console.log("Index: postChat OK", response);
+      })
+      .catch((error) => {
+        console.error("Index: postChat NG", error);
+      });
 
     provide(userInfoKey, userInfo);
     provide(messageListKey, messageList);
